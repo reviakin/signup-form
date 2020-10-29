fbdfc7b3d21502743d1a41d4f1fc3b94453241e4
reviakin <d.reviakin@8bridge.org>
2020-10-29 21:13:56 +0200

    add value, changeHandler,name props

--- src/TextInput.tsx
+++ src/TextInput.tsx
@@ -3,10 +3,25 @@ import React, { FC } from "react";
 type Props = {
   type: "text" | "password" | "email";
   placeholder: string;
+  value: string;
+  changeHandler: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  name: string;
 };
 
-const TextInput: FC<Props> = ({ type, placeholder }) => (
-  <input type={type} placeholder={placeholder} />
+const TextInput: FC<Props> = ({
+  type,
+  placeholder,
+  value,
+  changeHandler,
+  name,
+}) => (
+  <input
+    type={type}
+    placeholder={placeholder}
+    value={value}
+    onChange={changeHandler}
+    name={name}
+  />
 );
 
 export { TextInput };
