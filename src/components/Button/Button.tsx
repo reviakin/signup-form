3df22e4b9dccb3801b5f24977233d5ac29342136
reviakin <d.reviakin@8bridge.org>
2020-10-31 21:21:31 +0200

    add some style

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,17 +4,37 @@ type Props = {
   disabled?: boolean;
   label: string;
   background?: string;
+  width: number;
+  height: number;
+  type?: "rounded";
 };
 
 export type ButtonProps = PropsWithChildren<Props>;
+
+const disabledBGD = "#A2A2A2";
 
 const Button: FC<ButtonProps> = ({
   disabled,
   label,
   children,
   background = "#0094FF",
+  width = 120,
+  height = 20,
+  type,
 }) => (
-  <button type="submit" disabled={disabled} style={{ background }}>
+  <button
+    type="submit"
+    disabled={disabled}
+    style={{
+      background: disabled ? disabledBGD : background,
+      width,
+      height,
+      borderRadius: type === "rounded" ? "31px" : "",
+      color: "#ffffff",
+      border: "none",
+      cursor: disabled ? "not-allowed" : "pointer",
+    }}
+  >
     {label}
     {children}
   </button>
