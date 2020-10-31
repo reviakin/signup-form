238728e5e41b3b019f08d245bea5a2e5c2fef0d6
reviakin <d.reviakin@8bridge.org>
2020-10-31 21:04:20 +0200

    add label for stories

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from "@storybook/react/types-6-0";
 
 import { Button, ButtonProps } from "./Button";
@@ -16,5 +15,5 @@ const Template: Story<ButtonProps> = (args) => <Button {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {
-  label: "hui",
+  label: "Button",
 };
