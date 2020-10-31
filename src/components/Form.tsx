5185f6a89998d99fd2b506e5df8dd6cd048534ce
reviakin <d.reviakin@8bridge.org>
2020-10-31 12:10:48 +0200

    add submit prop

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,70 +1,66 @@
 import React, { FC } from "react";
 import { TextInput } from "./TextInput";
-import { Title } from "./Title";
-import { map } from "lodash/fp";
+import { compose, map } from "lodash/fp";
 import { RadioInput } from "./RadioInput";
 import { useForm } from "../tools";
+import { State } from "../tools/hooks/types";
 
-type Props = {};
+type Props = {
+  submit: (state: State) => void;
+};
 
-const Form: FC<Props> = (props) => {
+const Form: FC<Props> = ({ submit }) => {
   const [state, { change }] = useForm();
 
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) =>
+    compose(
+      () => submit(state),
+      (event: React.FormEvent<HTMLFormElement>) => event.preventDefault()
+    )(event);
+
   return (
-    <div
-      style={{
-        width: 400,
-        height: 605,
-        background: "#ffffff",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        borderRadius: "8px",
-      }}
-    >
-      <Title text="Create a new account" />
-      <form action="" method="post">
-        {map(
-          (input) =>
-            input.type !== "radio" ? (
-              <div key={input.type}>
-                <TextInput
-                  type={input.type}
-                  placeholder={input.placeholder}
-                  changeHandler={change}
-                  name={input.name}
-                  value={input.value}
-                  required={input.validation.required}
-                  minLength={input.validation.minLength}
-                  pattern={input.validation.pattern}
-                />
-              </div>
-            ) : (
-              <RadioInput
-                key={input.type}
+    <form onSubmit={onSubmit}>
+      {map(
+        (input) =>
+          input.type !== "radio" ? (
+            <div key={input.name}>
+              <TextInput
+                type={input.type}
+                placeholder={input.placeholder}
+                changeHandler={change}
                 name={input.name}
-                options={input.options}
                 value={input.value}
-                onChange={change}
               />
-            ),
-          state.inputs
-        )}
-        <div>
-          <select name="" id="">
-            <option value=""></option>
-          </select>
-        </div>
-        <div>
-          <input type="checkbox" name="" id="" />
-          <label htmlFor="">accept terms and conditions</label>
-        </div>
-        <div>
-          <button type="button">Sign up</button>
-        </div>
-      </form>
-    </div>
+            </div>
+          ) : (
+            <RadioInput
+              key={input.name}
+              name={input.name}
+              options={input.options}
+              value={input.value}
+              onChange={change}
+            />
+          ),
+        state.inputs
+      )}
+      <div>
+        <select name="" id="">
+          <option value=""></option>
+        </select>
+      </div>
+      <div>
+        <input type="checkbox" name="" id="" />
+        <label htmlFor="">accept terms and conditions</label>
+      </div>
+      <div>
+        <button
+          type="submit"
+          // disabled={!state.isValid}
+        >
+          Sign up
+        </button>
+      </div>
+    </form>
   );
 };
 
