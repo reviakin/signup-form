d1f5059914de7519c69d791882825d17cc834622
reviakin <d.reviakin@8bridge.org>
2020-10-31 11:33:30 +0200

    create useForm

---
+++ src/tools/hooks/useForm.ts
@@ -0,0 +1,73 @@
+import { useReducer, useCallback } from "react";
+
+import { map } from "lodash/fp";
+
+import { State, Actions } from "./types";
+
+const initState: State = [
+  {
+    type: "text",
+    placeholder: "name",
+    value: "",
+    name: "name",
+    validation: {
+      pattern: "[a-zA-Z]+",
+      required: true,
+    },
+  },
+  {
+    type: "email",
+    placeholder: "email",
+    value: "",
+    name: "email",
+    validation: {
+      pattern: "[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,}$",
+      required: true,
+    },
+  },
+  {
+    type: "password",
+    placeholder: "password",
+    value: "",
+    name: "password",
+    validation: {
+      pattern: "",
+      required: true,
+      minLength: 6,
+    },
+  },
+  {
+    type: "radio",
+    name: "gender",
+    options: ["male", "female"],
+    value: null,
+  },
+];
+
+const reducer = (state: State, action: Actions): State => {
+  if (action.type === "UPDATE_VALUE") {
+    const { name, value } = action.payload;
+    return map(
+      (input) => (input.name === name ? { ...input, value } : input),
+      state
+    );
+  }
+  return state;
+};
+
+function useForm(): [
+  State,
+  { change: (event: React.ChangeEvent<HTMLInputElement>) => void }
+] {
+  const [state, dispatch] = useReducer(reducer, initState);
+
+  const onInputChangeHandler = useCallback(
+    ({ target: { name, value } }: React.ChangeEvent<HTMLInputElement>) =>
+      dispatch({ type: "UPDATE_VALUE", payload: { name, value } }),
+    []
+  );
+
+  return [state, { change: onInputChangeHandler }];
+}
+
+export { useForm };
