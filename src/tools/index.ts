768d7fd4c6420bac3c1bb20b420aa617accd4dd7
reviakin <d.reviakin@8bridge.org>
2020-10-31 11:33:57 +0200

    add useForm to imp

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -1,5 +1,6 @@
 import { Box } from "./Box";
 import { Right, Left } from "./RightLeft";
 import { logIt } from "./logIt";
+import { useForm } from "./hooks";
 
-export { Box, Right, Left, logIt };
+export { Box, Right, Left, logIt, useForm };
