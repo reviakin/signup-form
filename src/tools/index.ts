eb6bd89cb4e5ce04bf8970a9c676f9c931333328
reviakin <d.reviakin@8bridge.org>
2020-10-31 15:48:53 +0200

    add func to tools' import/export

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -2,5 +2,7 @@ import { Box } from "./Box";
 import { Right, Left } from "./RightLeft";
 import { logIt } from "./logIt";
 import { useForm } from "./hooks";
+import { apolloClient } from "./apollo";
+import { tryCatch } from "./tryCatch";
 
-export { Box, Right, Left, logIt, useForm };
+export { Box, Right, Left, logIt, useForm, tryCatch, apolloClient };
