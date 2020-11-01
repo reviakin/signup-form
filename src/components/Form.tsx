e8b67602ee2b295db6b8977e80516177a113d4ab
reviakin <d.reviakin@8bridge.org>
2020-11-01 11:10:51 +0200

    add Inputs

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,12 +1,12 @@
 import React, { FC } from "react";
-import { TextInput } from "./TextInput";
 import { compose, map, merge, reduce } from "lodash/fp";
-import { RadioInput } from "./RadioInput";
-import { useForm } from "../tools";
-import { Input } from "../tools/hooks/types";
-import { Box } from "../tools";
+
 import { Loader } from "./Loader";
 import { Button } from "./Button";
+import { Inputs } from "./Inputs";
+
+import { Input } from "../tools/hooks/types";
+import { Box, useForm } from "../tools";
 
 type Props = {
   submit: (a: any) => void;
@@ -32,34 +32,7 @@ const Form: FC<Props> = ({ submit, inputs, loading }) => {
 
   return (
     <form onSubmit={onSubmit}>
-      {map(
-        (input) =>
-          input.type !== "radio" ? (
-            <div key={input.name}>
-              <TextInput
-                type={input.type}
-                placeholder={input.placeholder}
-                changeHandler={change}
-                name={input.name}
-                value={input.value}
-              />
-            </div>
-          ) : (
-            <RadioInput
-              key={input.name}
-              name={input.name}
-              options={input.options}
-              value={input.value}
-              onChange={change}
-            />
-          ),
-        state.inputs
-      )}
-      <div>
-        <select name="" id="">
-          <option value=""></option>
-        </select>
-      </div>
+      <Inputs inputs={state.inputs} change={change} />
       <div>
         <input type="checkbox" name="" id="" />
         <label htmlFor="">accept terms and conditions</label>
