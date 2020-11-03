2d34aecb4d051c2859ed8b5e01bc8ea88b23cf64
reviakin <d.reviakin@8bridge.org>
2020-11-03 17:46:13 +0200

    replace radiobutton to fld

---
+++ src/components/RadioInput/RadioInput.tsx
@@ -0,0 +1,41 @@
+import React, { FC } from "react";
+import { map } from "lodash/fp";
+
+type Props = {
+  name: string;
+  options: string[];
+  onChange: (input: { name: string; value: string }) => void;
+  value: null | string;
+  classname?: string;
+};
+
+const RadioInput: FC<Props> = ({
+  name,
+  options,
+  onChange,
+  value,
+  classname,
+}) => (
+  <>
+    {map(
+      (option) => (
+        <div key={option}>
+          <input
+            className={classname}
+            onClick={() => onChange({ name, value: option })}
+            type="radio"
+            name={name}
+            value={option}
+            checked={option === value}
+          />
+          <label>
+            {option.charAt(0).toUpperCase() + option.slice(1).toLowerCase()}
+          </label>
+        </div>
+      ),
+      options
+    )}
+  </>
+);
+
+export { RadioInput };
