e473557eba1e1ffc74c43b453bee1a9ede2e4bc7
reviakin <d.reviakin@8bridge.org>
2020-11-03 10:21:45 +0200

    fixed style props in Button's stories

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -16,14 +16,18 @@ const Template: Story<ButtonProps> = (args) => <Button {...args} />;
 export const Primary = Template.bind({});
 Primary.args = {
   label: "Button",
-  width: 343,
-  height: 62,
+  style: {
+    width: 343,
+    height: 62,
+  },
 };
 
 export const Rounded = Template.bind({});
 Rounded.args = {
   label: "Button",
-  width: 343,
-  height: 62,
+  style: {
+    width: 343,
+    height: 62,
+  },
   type: "rounded",
 };
