e77a9f635240ec46048c373054ae46bd29e0fa62
reviakin <d.reviakin@8bridge.org>
2020-11-03 20:07:57 +0200

    css prop

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,9 +1,11 @@
 import React, { CSSProperties, FC } from "react";
 
-type Props = { text: string; style?: CSSProperties };
+type Props = { text: string; style?: CSSProperties; classname?: string };
 
-const Title: FC<Props> = ({ text, style }) => (
-  <p style={{ margin: 0, padding: 0, ...style }}>{text}</p>
+const Title: FC<Props> = ({ text, style, classname }) => (
+  <p style={{ margin: 0, padding: 0, ...style }} className={classname}>
+    {text}
+  </p>
 );
 
 export { Title };
